fe70ce7584cc5d191501acc1cd74c57712f5e998
andrejmihail <andrejmihail8@gmail.com>
2023-03-06 23:54:59 +0500

    IsValid

--- Easy.cs
+++ Easy.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -35,5 +35,44 @@ namespace leetCodeTrening
             }         
             return sum;
         }
+<<<<<<< Updated upstream
+=======
+
+        /// <summary>
+        /// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input
+        /// string is valid.An input string is valid if: Open brackets must be closed by the same type of brackets. Open
+        /// brackets must be closed in the correct order. Every close bracket has a corresponding open bracket of the
+        /// same type. <code>Example 1: Input: s = "()" Output: true   
+        /// Example 2: Input: s = "(]" Output: false</code>
+        /// </summary>
+
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++) 
+            {
+                char ch = s[i];
+                switch (ch)
+                {
+                    case '(':
+                        stack.Push(')');
+                        break;
+                    case '{':
+                        stack.Push('}');
+                        break;
+                    case '[':
+                        stack.Push(']');
+                        break;
+                    case ']':
+                    case '}':
+                    case ')':
+                        if (stack.Count == 0) return false;
+                        if (stack.Pop() != ch ) return false;
+                        break;
+                }                               
+            } 
+            return stack.Count == 0;
+        }
+>>>>>>> Stashed changes
     }
 }
