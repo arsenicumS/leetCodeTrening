98d3c2b86a57ca09ad765b70e83395694f9f30a6
andrejmihail <andrejmihail8@gmail.com>
2023-03-06 19:17:37 +0500

    Добавьте файлы проекта.

---
+++ Easy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCodeTrening
+{
+    internal class Easy
+    {
+        /// <summary>
+        /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.You may assume that each input would have exactly one solution, and you may not use the same element twice.You can return the answer in any order. 
+        ///<code> 
+        ///Input: nums = [2,7,11,15], target = 9      
+        /// Output: [0,1]        
+        /// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+        /// </code>
+        /// </summary>        
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] sum = new int[2];
+            for (int i = 0; i < nums.Length; i++) {
+                int index = Array.IndexOf(nums, target - nums[i]);
+                if (index != -1) {
+                    if (i < index)
+                    {
+                        sum[0] = i;
+                        sum[1] = index;
+                        break;
+                    }
+                    sum[0] = index;
+                    sum[1] = i;
+                }
+            }         
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCodeTrening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Easy easy = new Easy();
+            int [] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            foreach(int i in easy.TwoSum(nums, target)) {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
